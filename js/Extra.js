8bccdf286d798112f4595d4f0f9086b5f3ccb3d1
Martin Donk <martin.r.donk@gmail.com>
2017-05-19 06:30:07 -0500

    Updating libs

--- js/Extra.js
+++ js/Extra.js
@@ -15,32 +15,46 @@ if((typeof module) !== 'undefined') {
     var core = nerdamer.getCore(),
         _ = core.PARSER,
         Symbol = core.Symbol,
+        format = core.Utils.format,
         S = core.groups.S,
         EX = core.groups.EX;
+    
+    
     
     var __ = core.Extra = {
         version: '1.0.0',
         //http://integral-table.com/downloads/LaplaceTable.pdf
         LaPlace: {
+            //Using: intgral_0_oo f(t)*e^(-s*t) dt
             transform: function(symbol, t, s) {
-                var u = '__u__';
-                var sym = symbol.sub(t, u);
-                if(!sym.contains(u, true)) 
-                    retval = _.divide(symbol, s.clone());
-                else {
-                    //e^(a*t)
-                    if(sym.isE()) {
-                        var tmp = _.parse('e^(-'+s+'*'+u+')*'+sym);
-                        //TODO: I have to find a way to put in into more familar form
-                        retval = core.Calculus.integrate(tmp, u).sub(u, 0);
+                var integration_depth = core.Settings.integration_depth; //cache the depth
+                //Laplace transforms dig in deep and need to make way more stack call than other integration
+                core.Settings.integration_depth = 20;
+                //This has to run in a block where PARSE2NUMBER = false
+                var result = core.Utils.block('PARSE2NUMBER', function() {
+                    //special cases
+                    symbol = Symbol.unwrapSQRT(symbol, true);
+                    var retval;
+                    if(symbol.group === S && symbol.power.equals(1/2)) {
+                        return _.parse(format('({0})*sqrt(pi)/(2*({1})^(3/2))', symbol.multiplier, s));
                     }
-                }
-                
-                return retval.sub(u, t);
+                    else {
+                        var u = 't';
+                        var sym = symbol.sub(t, u);
+                        retval = core.Calculus.integrate(_.parse('e^(-'+s+'*'+u+')*'+sym), u).sub(u, 0);
+                        retval = _.expand(_.multiply(retval, new Symbol(-1)));
+                        retval = retval.sub(u, t);
+                    }
+                    return core.Utils.block('PARSE2NUMBER', function() {
+                        return _.parse(retval);
+                    }, true);   
+                    //return retval;
+                    
+                }, false);
+                //put back the integration depth as you found it
+                core.Settings.integration_depth = integration_depth;
+                return result;
             }
-        },
-        Fourier: {
-            
         },
         Statistics: {
             frequencyMap: function(arr) {
@@ -131,17 +145,32 @@ if((typeof module) !== 'undefined') {
                 else
                     retval = _.parse(max[0]);
                 return retval;
+            }, 
+            gVariance: function(k, args) {
+                var x_ = __.Statistics.mean.apply(__.Statistics, args),
+                    sum = __.Statistics.sum(args, x_);
+                return _.multiply(k, sum);
+            },
+            variance: function() {
+                var args = [].slice.call(arguments),
+                    k = _.divide(new Symbol(1), __.Statistics.count(args));
+                return __.Statistics.gVariance(k, args);
             },
             sampleVariance: function() {
                 var args = [].slice.call(arguments),
-                    k = _.divide(new Symbol(1), _.subtract(__.Statistics.count(args), new Symbol(1))),
-                    x_ = __.Statistics.mean.apply(__.Statistics, args),
-                    sum = __.Statistics.sum(args, x_);
-                return _.multiply(k, sum);
+                    k = _.divide(new Symbol(1), _.subtract(__.Statistics.count(args), new Symbol(1)));
+                return __.Statistics.gVariance(k, args);
+            },
+            standardDeviation: function() {
+                var args = [].slice.call(arguments);
+                return _.pow(__.Statistics.variance.apply(__.Statistics, args), new Symbol(1/2));
             },
             sampleStandardDeviation: function() {
                 var args = [].slice.call(arguments);
                 return _.pow(__.Statistics.sampleVariance.apply(__.Statistics, args), new Symbol(1/2));
+            },
+            zScore: function(x, mean, stdev) {
+                return _.divide(_.subtract(x, mean), stdev);
             }
         },
         functions: {
@@ -182,11 +211,25 @@ if((typeof module) !== 'undefined') {
             build: function() { return __.Statistics.sampleVariance; }
         },
         {
+            name: 'variance',
+            visible: true,
+            numargs: -1,
+            build: function() { return __.Statistics.variance; }
+        },
+        {
             name: 'smpstdev',
             visible: true,
             numargs: -1,
             build: function() { return __.Statistics.sampleStandardDeviation; }
         },
+        {
+            name: 'stdev',
+            visible: true,
+            numargs: -1,
+            build: function() { return __.Statistics.standardDeviation; }
+        }
     ]);
     
+    //link registered functions externally
+    nerdamer.api();
 }());
