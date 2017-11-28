85c1dd96d78f0ca851f75b97f2125d166803f428
Happypig375 <hadrianwttang@outlook.com>
2017-11-28 18:41:23 +0800

    Fix for lcm not being exposed

--- Algebra.js
+++ Algebra.js
@@ -2733,6 +2733,12 @@ if((typeof module) !== 'undefined') {
             build: function() { return __.gcd; }
         },
         {
+            name: 'lcm',
+            visible: true,
+            numargs: 2,
+            build: function() { return __.lcm; }
+        },
+        {
             name: 'roots',
             visible: true,
             numargs: -1,
