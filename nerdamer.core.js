8ebdc89f789d06b385b88d1a260dae6dc158978a
mdonk <mdonk@gmail.com>
2018-01-16 11:35:39 -0600

    update sort

--- nerdamer.core.js
+++ nerdamer.core.js
@@ -5368,17 +5368,21 @@ var nerdamer = (function(imports) {
         }
         
         function sort(symbol, opt) {
-            opt = opt ? opt.toString() : 'asc';
+            opt = opt ? opt.toString() : 'asc'; 
             var getval = function(e) {
+                if(e.group === N)
+                    return e.multiplier;
                 if(e.group === FN) {
                     if(e.fname === '')
                         return getval(e.args[0]);
                     return e.fname;
                 }
+                if(e.group === S)
+                    return e.power;
                 
                 return e.value;
             };
-            var symbols = symbol.collectSymbols();
+            var symbols = isVector(symbol) ? symbol.elements : symbol.collectSymbols();
             return new Vector(symbols.sort(function(a, b) {
                 var aval = getval(a),
                     bval = getval(b);
@@ -8530,6 +8534,3 @@ var nerdamer = (function(imports) {
 if((typeof module) !== 'undefined') {
     module.exports = nerdamer;
 };
-
-var x = nerdamer('(1/2)!');
-console.log(x.text())
