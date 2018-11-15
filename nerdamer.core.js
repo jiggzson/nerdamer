ca0f3b03c47fe873dfe4cf40e6fc057dffae2f8d
Martin Donk <jiggzson@gmail.com>
2018-11-14 19:40:43 -0600

    Updating text function ... failing text.spec

--- nerdamer.core.js
+++ nerdamer.core.js
@@ -1592,8 +1592,81 @@ var nerdamer = (function(imports) {
      */
     function text(obj, option, useGroup) { 
         var asHash = option === 'hash',
-            asDecimal = option === 'decimals' || option === 'decimal',
+            //whether to wrap numbers in brackets
+            wrapCondition = undefined,
             opt = asHash ? undefined : option;
+        
+        function toString(obj) {
+            switch(option)
+            {
+                case 'decimals':
+                case 'decimal':
+                    wrapCondition = wrapCondition || function(str) { return false; };
+                    return obj.valueOf();
+                case 'recurring':
+                    wrapCondition = wrapCondition || function(str) { return str.indexOf("'") !== -1; };
+                    
+                    var str = obj.toString();
+                    //verify that the string is actually a fraction
+                    var frac = /^-?\d+(?:\/\d+)?$/.exec(str);
+                    if(frac.length == 0) return str;
+                    
+                    //split the fraction into the numerator and denominator
+                    var parts = frac[0].split('/');
+                    var negative = false;
+                    var m = Number(parts[0]);
+                    if(m < 0) { m = -m; negative = true; }
+                    var n = Number(parts[1]);
+                    if(!n) n = 1;
+
+                    //https://softwareengineering.stackexchange.com/questions/192070/what-is-a-efficient-way-to-find-repeating-decimal#comment743574_192081
+                    var quotient = Math.floor(m / n), c = 10 * (m - quotient * n);
+                    quotient = quotient.toString() + ".";
+                    while(c && c < n) {
+                        c *= 10;
+                        quotient += "0";
+                    }
+                    var digits = "", passed = [], i = 0;
+                    while(true) {
+                        if(typeof passed[c] !== 'undefined') {
+                            var prefix = digits.slice(0, passed[c]),
+                                cycle = digits.slice(passed[c]),
+                                result = quotient + prefix + "'" + cycle + "'";
+                            return (negative ? "-" : "") + result.replace("'0'", "").replace(/\.$/, "");
+                        }
+                        var q = Math.floor(c / n), r = c - q * n;
+                        passed[c] = i;
+                        digits += q.toString();
+                        i += 1;
+                        c = 10 * r;
+                    }
+                case 'mixed':
+                    wrapCondition = wrapCondition || function(str) { return str.indexOf('/') !== -1; };
+                
+                    var str = obj.toString();
+                    //verify that the string is actually a fraction
+                    var frac = /^-?\d+(?:\/\d+)?$/.exec(str);
+                    if(frac.length == 0) return str;
+                    
+                    //split the fraction into the numerator and denominator
+                    var parts = frac[0].split('/');
+                    var numer = new bigInt(parts[0]);
+                    var denom = new bigInt(parts[1]);
+                    if(denom.equals(0)) denom = new bigInt(1);
+                    
+                    //return the quotient plus the remainder
+                    var divmod = numer.divmod(denom);
+                    var quotient = divmod.quotient;
+                    var remainder = divmod.remainder;
+                    var operator = parts[0][0] === '-' || quotient.equals(0) || remainder.equals(0) ? '' : '+';
+                    return (quotient.equals(0) ? '' : quotient.toString()) + operator + (remainder.equals(0) ? '' : (remainder.toString() + '/' + parts[1]));
+                default:
+                    wrapCondition = wrapCondition || function(str) { return str.indexOf('/') !== -1; };
+                    
+                    return obj.toString();
+            }
+        }
+        
         //if the object is a symbol
         if(isSymbol(obj)) { 
             var multiplier = '', 
@@ -1604,17 +1677,17 @@ var nerdamer = (function(imports) {
             //if the value is to be used as a hash then the power and multiplier need to be suppressed
             if(!asHash) { 
                 //use asDecimal to get the object back as a decimal
-                var om = asDecimal ? obj.multiplier.valueOf() : obj.multiplier.toString();
-                if(Number(om) === -1) {
+                var om = toString(obj.multiplier);
+                if(om == '-1') {
                     sign = '-';
                     om = '1';
                 }
                 //only add the multiplier if it's not 1
-                if(Number(om) !== 1) multiplier = om;
+                if(om != '1') multiplier = om;
                 //use asDecimal to get the object back as a decimal
-                var p = obj.power ? (asDecimal ? obj.power.valueOf() : obj.power.toString()) : '';
+                var p = obj.power ? toString(obj.power) : '';
                 //only add the multiplier 
-                if(String(p) !== '1') {
+                if(p != '1') {
                     //is it a symbol
                     if(isSymbol(p)) {
                         power = text(p, opt);
@@ -1629,7 +1702,7 @@ var nerdamer = (function(imports) {
                 case N:
                     multiplier = '';
                     //if it's numerical then all we need is the multiplier
-                    value = obj.multiplier == '-1' ? '1' : asDecimal ? obj.valueOf() : obj.multiplier.toString();
+                    value = obj.multiplier == '-1' ? '1' : toString(obj.multiplier);
                     power = '';
                     break;
                 case PL:
@@ -1663,7 +1736,7 @@ var nerdamer = (function(imports) {
                     break;
             }
             
-            if(group === FN && asDecimal) { 
+            if(group === FN) { 
                 value = obj.fname+inBrackets(obj.args.map(function(symbol) {
                     return text(symbol, opt);
                 }).join(','));
@@ -1680,51 +1753,39 @@ var nerdamer = (function(imports) {
             }
             //wrap the power since / is less than ^
             //TODO: introduce method call isSimple
-            if(power && !isInt(power) && group !== EX && !asDecimal) { power = inBrackets(power); }
+            if(power && group !== EX && wrapCondition(power)) { power = inBrackets(power); }
 
             //the following groups are held together by plus or minus. They can be raised to a power or multiplied
             //by a multiplier and have to be in brackets to preserve the order of precedence
-            if(((group === CP || group === PL) && (multiplier && Number(multiplier) !== 1 || sign === '-')) 
-                    || ((group === CB || group === CP || group === PL) && (power && Number(power) !== 1))
-                    || !asHash && group === P && Number(value) === -1
+            if(((group === CP || group === PL) && (multiplier && multiplier != '1' || sign === '-')) 
+                    || ((group === CB || group === CP || group === PL) && (power && power != '1'))
+                    || !asHash && group === P && value == -1
                     || obj.fname === PARENTHESIS) { 
                 
                 value = inBrackets(value);
             }
             
             var c = sign+multiplier;
-            if(multiplier && !isInt(multiplier) && !asDecimal) 
-                c = inBrackets(c);
+            if(multiplier && wrapCondition(multiplier)) c = inBrackets(c);
             
-            if(power < 0) 
-                power = inBrackets(power);
-            if(multiplier) 
-                c = c + '*';
-            if(power) 
-                power = Settings.POWER_OPERATOR + power;
+            if(power < 0) power = inBrackets(power);
+            if(multiplier) c = c + '*';
+            if(power) power = Settings.POWER_OPERATOR + power;
 
             //this needs serious rethinking. Must fix
-            if(group === EX && value.charAt(0) === '-') 
-                value = inBrackets(value);
+            if(group === EX && value.charAt(0) === '-') value = inBrackets(value);
             
             var cv = c+value;
             
-            if(obj.parens) 
-                cv = inBrackets(cv);
+            if(obj.parens) cv = inBrackets(cv);
 
             return cv+power;
         }
         else if(isVector(obj)) { 
             var l = obj.elements.length,
                 c = [];
-            for(var i=0; i<l; i++) {
-                
-                c.push(obj.elements[i].text(option));
-            }
-            var s = '['+c.join(',')+']';
-            if(obj.getter)
-                s = s+'['+text(obj.getter)+']';
-            return s;
+            for(var i=0; i<l; i++) c.push(obj.elements[i].text(option));
+            return '['+c.join(',')+']';
         }
         else {
             try {
@@ -9142,4 +9203,3 @@ var nerdamer = (function(imports) {
 if((typeof module) !== 'undefined') {
     module.exports = nerdamer;
 };
-
