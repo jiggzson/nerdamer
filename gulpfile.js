a34c0f65fa871989ecaa759711b9f486e8ad8313
Martin Donk <jiggzson@gmail.com>
2021-04-27 08:45:31 -0500

    Added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+var devFiles = ['nerdamer.core.js','./Algebra.js', './Calculus.js', './Solve.js', './Extra.js'];
+
 gulp.task('concat_all', function() {
-    return gulp.src(['./nerdamer.core.js','./Algebra.js', './Calculus.js', './Solve.js', './Extra.js'])
+    return gulp.src(devFiles)
         .pipe(concat('all.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(devFiles, gulp.series('concat_all'));
+}); 
